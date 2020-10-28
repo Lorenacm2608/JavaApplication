909a5de13256aa7ec733a336bf72281d968509ac
2dam <2dam@HZ359585.TartangaLH.eus>
2020-10-28 09:56:53 +0100

    Worker terminado--ApplicationServer.

--- src/mainServer/ApplicationServer.java
+++ src/mainServer/ApplicationServer.java
@@ -1,12 +1,30 @@
+
 package mainServer;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import worker.Worker;
 
 /**
  *
- * @author 2dam
+ * @author Fredy Vargas Flores
+ * @author Lorena Cáceres Manuel
  */
 public class ApplicationServer {
-  public static void main(String[] args) {
-        
-    }  
-    
+
+    private static final int port = 5555;
+    //!--AÑADIR LIMITE DE HILOS
+    public static void main(String[] args) throws IOException {
+       new ApplicationServer().iniciarServer();
+       
+}
+    public void iniciarServer() throws IOException{
+        ServerSocket ss = new ServerSocket(port);
+        System.out.println("[SERVER] Iniciando Servidor...");
+        while(true){
+            Socket socket = ss.accept(); //Aceptaremos al cliente
+            System.out.println("[SERVER] Cliente aceptado!");
+            new Worker (socket).start(); //Llamamos al worker y le pasamos el socketS
+        }
+    }
 }
--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -1,9 +1,47 @@
 package worker;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Libreries.Usuario;
 
 /**
  *
- * @author 2dam
+ * @author Lorena Cáceres Manuel
+ * @author Fredy Vargas Flores
  */
-public class Worker {
+public class Worker extends Thread {
+
+    private Socket socket=null;
+ 
+    public Worker(Socket Sclient) {
+            this.socket = Sclient;  
+    }
+    
+    public void run(){
+        try {
+            //Declaramos el flujo de entrada de datos del Socket
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream()); 
+            //Declaramos el flujo de salida de datos del Socket
+            ObjectOutputStream oos = new ObjectOutputStream (socket.getOutputStream());
+            //Instanciamos un Objeto de tipo Usuario y lo leemos
+            Usuario usuario = (Usuario) ois.readObject();
+            //Lo sacamos por pantalla 
+            System.out.println("Me llega el servidor el nombre " + usuario.getUsuario());
+            //Instanciamos otro Objeto de tipo Usuario y le pasamos el String Tartanga
+            Usuario usuarioR = new Usuario("Tartanga");
+            //Escribimo el objeto que hemos instanciado anteriormente -- Sacará por pantalla (Cliente): Tartanga
+            oos.writeObject(usuarioR);
+            //Cerramos el Socket
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     
 }
