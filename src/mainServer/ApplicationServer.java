0fd2d6f25e2aec8f2ceb2cf3575f5fd723f6e4bb
fredy <fredy@dam.com>
2020-11-04 06:12:51 +0100

    MultiHilo corregido (SERVER-WORKER)

--- src/mainServer/ApplicationServer.java
+++ src/mainServer/ApplicationServer.java
@@ -3,6 +3,10 @@ package mainServer;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import worker.Worker;
@@ -14,9 +18,11 @@ import worker.Worker;
  */
 public class ApplicationServer {
 
-    private int limiteHilo = 2;
-    private int actualHilo = 0;
+  
+    private static int actualHilo = 0;
     private static final int port = 5555;
+     static int MAX_HILO = 1;
+   
 
     //!--AÑADIR LIMITE DE HILOS
     public static void main(String[] args) throws IOException {
@@ -25,25 +31,41 @@ public class ApplicationServer {
     }
 
     public void iniciarServer() throws IOException {
+       
         ServerSocket ss = new ServerSocket(port);
         System.out.println("[SERVER] Iniciando Servidor...");
-        int hActuales = 0;
+           
         while (true) {
             Socket socket = ss.accept(); //Aceptaremos al cliente
-
-            if (actualHilo < limiteHilo) {
-                System.out.println("[SERVER] Cliente aceptado!");
-                Worker worker = new Worker(socket); //Llamamos al worker y le pasamos el socket
-                actualHilo++;
-                try {
-                    worker.join();
-                    actualHilo--;
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(ApplicationServer.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            } else {
-                System.out.println("Siento decirte que no hay hilos disponibles ");
+            System.out.println("[SERVER] Cliente aceptado!");
+            incrementarHilo();
+                    Worker worker1= new Worker(socket);
+            if(control()){
+                
+                    incrementarHilo();
+                    Worker worker= new Worker(socket);
+                     
+                
+            }else{
+                System.out.println("No hay hilos disponibles");
             }
-        }
+            
+        }      
     }
+    public synchronized boolean control(){
+        boolean resu=false;
+        if(actualHilo<MAX_HILO){
+            resu=true;
+        }
+        return resu;
+    }
+    public synchronized  void incrementarHilo(){
+        actualHilo++;
+        System.out.println("total "+actualHilo);
+    }
+    synchronized static public void liberarHilo(){
+        actualHilo--;
+        System.out.println("total "+actualHilo);
+    }
+
 }
--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -10,13 +10,16 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Application;
 import libreries.Message;
 import libreries.Signable;
 import libreries.Tipo;
 import libreries.Usuario;
 import libreries.SignableFactory;
+import mainServer.ApplicationServer;
 
 /**
  *
@@ -26,69 +29,75 @@ import libreries.SignableFactory;
 public class Worker extends Thread {
 
     private Socket socket = null;
+    private Message msg;
 
     public Worker() {
 
     }
 
-    public Worker(Socket Sclient) {
-        this.socket = Sclient;
+    public Worker(Socket sClient) {
+        this.socket = sClient;
         this.start();
     }
 
     @Override
     public void run() {
-        try {
-            Message msg = null;
-            ObjectOutputStream oos = null;
-            ObjectInputStream ois = null;
+       
             try {
+                Message msg = null;
+                ObjectOutputStream oos = null;
+                ObjectInputStream ois = null;
 
-                // Signable dao = new SignableFactory().getSignableImplementation;
-                DaoImplementation dao = new DaoImplementation();
-                //Declaramos el flujo de entrada de datos del Socket
-                ois = new ObjectInputStream(socket.getInputStream());
-                oos = new ObjectOutputStream(socket.getOutputStream());
-                msg = new Message();
-                msg = (Message) ois.readObject();
-                //
-                //Instanciamos un Objeto de tipo Usuario y lo leemos
-                System.out.println(msg.getTipoMensaje() + " + " + msg.getUsuario().getUsuario());
-                switch (msg.getTipoMensaje()) {
-                    case LOGIN:
-                        dao.logIn(msg.getUsuario());
-                        System.out.println("dao hecho");
-                        msg.setTipoMensaje(Tipo.OK);
-                        System.out.println("flujo escrito");
-                        break;
-                    case SIGNUP:
-                        dao.signUp(msg.getUsuario());
-                        System.out.println("dao hecho");
-                        msg.setTipoMensaje(Tipo.OK);
-                        System.out.println("flujo escrito");
-                        break;
-                } 
-            } catch (ClassNotFoundException ex) {
-                msg.setTipoMensaje(Tipo.ERROR_BASE_DE_DATOS);
-            } catch (AutenticacionFallidaException ex) {
-               msg.setTipoMensaje(Tipo.CONTRASENA_ERRONEA);
-            } catch (ErrorBDException ex) {
-                msg.setTipoMensaje(Tipo.ERROR_BASE_DE_DATOS);
-            } catch (ErrorServerException ex) {
-                msg.setTipoMensaje(Tipo.ERROR_SERVIDOR);
-            } catch (UsuarioNoEncontradoException ex) {
-                msg.setTipoMensaje(Tipo.USUARIO_NO_EXISTE);
-            } catch (UsuarioExistenteException ex) {
-                msg.setTipoMensaje(Tipo.USUARIO_EXISTENTE);
+                try {
+                    DaoImplementation dao = new DaoImplementation();
+                    ois = new ObjectInputStream(socket.getInputStream());
+                    oos = new ObjectOutputStream(socket.getOutputStream());
+                    msg = new Message();
+                    msg = (Message) ois.readObject();
+
+                    System.out.println(msg.getTipoMensaje() + " + " + msg.getUsuario().getUsuario());
+                    switch (msg.getTipoMensaje()) {
+                        case LOGIN:
+                            dao.logIn(msg.getUsuario());
+                            System.out.println("dao hecho");
+                            msg.setTipoMensaje(Tipo.OK);
+                            System.out.println("flujo escrito");
+                            break;
+                        case SIGNUP:
+                            dao.signUp(msg.getUsuario());
+                            System.out.println("dao hecho");
+                            msg.setTipoMensaje(Tipo.OK);
+                            System.out.println("flujo escrito");
+                            break;
+                    }
+                } catch (ClassNotFoundException ex) {
+                    msg.setTipoMensaje(Tipo.ERROR_BASE_DE_DATOS);
+                } catch (AutenticacionFallidaException ex) {
+                    msg.setTipoMensaje(Tipo.CONTRASENA_ERRONEA);
+                } catch (ErrorBDException ex) {
+                    msg.setTipoMensaje(Tipo.ERROR_BASE_DE_DATOS);
+                } catch (ErrorServerException ex) {
+                    msg.setTipoMensaje(Tipo.ERROR_SERVIDOR);
+                } catch (UsuarioNoEncontradoException ex) {
+                    msg.setTipoMensaje(Tipo.USUARIO_NO_EXISTE);
+                } catch (UsuarioExistenteException ex) {
+                    msg.setTipoMensaje(Tipo.USUARIO_EXISTENTE);
+                } finally {
+                    oos.writeObject(msg);
+                    System.out.println(msg.getTipoMensaje());
+                    oos.close();
+                    ois.close();
+                    socket.close();
+
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
             } finally {
-                oos.writeObject(msg);
-                System.out.println(msg.getTipoMensaje());
-                oos.close();
-                ois.close();
-                socket.close();
+                    ApplicationServer.liberarHilo();                   
             }
-        } catch (IOException ex) {
-            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
+        
+
     }
+
 }
